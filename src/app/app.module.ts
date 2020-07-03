5aa69910b4e7eec70bf39179f1a435f319a52b62
Hilmar Tuneke <hilmar.tuneke@capgemini.com>
2020-07-04 01:39:28 +0200

    Aufgabe 3, Teil 1: Reducer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import {reducers} from "./reducers";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
